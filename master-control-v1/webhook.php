93b4c260a3b2cd91819bfb919e0f9a2a9977ea59
Forrest Fuqua <804052+jetbalsa@users.noreply.github.com>
2022-08-29 12:57:57 -0400

    Update webhook.php

--- master-control-v1/webhook.php
+++ master-control-v1/webhook.php
@@ -3,7 +3,7 @@ require "vendor/autoload.php";
 function logtodiscord($message)
 {
     $msg = $message;
-    $url = "https://discord.com/api/webhooks/990365476062375946/XPxdykos4kcI6zRvUtco9pMf17QDfZZrR0lSlgwM8ONmE5Ex5QDfB4DHaMpGGP9NSoV6/slack";
+    $url = "https://discord.com/api/webhooks/xxxx/slack";
     $useragent = "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)";
     $payload = 'payload={"username": "SODA", "text": "'.$msg.'"}';
      
